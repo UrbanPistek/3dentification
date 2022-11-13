a6013e5188e303b88f80ac3b5ee0537a6b514688
UrbanPistek <urban.pistek@gmail.com>
2022-11-13 11:36:50 -0500

    added closest color approximation

--- plastic-cv/webcam.py
+++ plastic-cv/webcam.py
@@ -72,18 +72,32 @@ def face_detect(vid, faceCascade):
     # displaying image with bounding box
     cv2.imshow('face_detect', frame)
 
+def closest_colour(requested_colour):
+    min_colours = {}
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+
 def get_colour_name(requested_colour):
     try:
-        closest_name = webcolors.rgb_to_name(requested_colour)
+        closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
     except ValueError:
-        closest_name = None
-    return closest_name
+        closest_name = closest_colour(requested_colour)
+        actual_name = None
+    return actual_name, closest_name
 
 def mean_colour(frame):
     average = frame.mean(axis=0).mean(axis=0)
 
     print(f"mean color: {average}")
-    closest_name = get_colour_name(average)
+    colors = (average[2], average[1], average[0])
+
+    actual_name, closest_name = get_colour_name(colors)
+    print ("mean actual colour name:", actual_name)
     print ("mean closest colour name:", closest_name)
 
 def dominant_color(frame):
@@ -100,7 +114,9 @@ def dominant_color(frame):
     dominant = palette[np.argmax(counts)]
 
     print(f"dominant color: {dominant}")
-    closest_name = get_colour_name(dominant)
+    actual_name, closest_name = get_colour_name(dominant)
+    print ("dominant closest colour name:", closest_name)
+    print ("dominant actual colour name:", actual_name)
     print ("dominant closest colour name:", closest_name)
 
 
