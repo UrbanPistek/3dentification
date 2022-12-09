c0868f46e3cedaaba60c2dafb6e30d0946f705bd
UrbanPistek <urban.pistek@gmail.com>
2022-12-09 16:26:21 -0500

    added sample code to read from arduino through python

--- plastic-identifier/scripts/read_serial.py
+++ plastic-identifier/scripts/read_serial.py
@@ -2,15 +2,29 @@
 import serial
 import time
 
-arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+from lib.utils import get_serial_ports
 
-def write_read():
-    arduino.write(bytes("scan", 'utf-8'))
+def write_read(dev: serial.Serial, msg):
+    dev.write(bytes(msg, 'utf-8'))
     
     time.sleep(0.5)
-    data = arduino.readline()
+    data = dev.readline()
     
     return data
 
-value = write_read()
-print(f"recieved: {value}")
+def main():
+
+    # Print out all availible ports
+    ports = get_serial_ports()
+    print(f"Availible Ports: \n{ports}\n")
+
+    arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
+
+    messages = ["ping", "adc", "scan"]
+    for msg in messages:
+        print(f"writing: {msg}")
+        value = write_read(arduino, msg)
+        print(f"recieved: {value}")
+
+if __name__ == "__main__":
+    main()
