4178625d75c2da2d224f733c89fae083eb2a3f2b
UrbanPistek <urban.pistek@gmail.com>
2022-12-27 14:06:21 -0500

    Added skeleton for system architecture with docker containers

---
+++ plastic-identifier/scripts/reader_agent.py
@@ -0,0 +1,14 @@
+"""DNIR Module Reader"""
+import os
+import time
+
+def main() -> None:
+
+    while True:
+        time.sleep(3)
+        print("DNIR Reader Agent...")
+
+if __name__ == "__main__":
+    main()
+
+
