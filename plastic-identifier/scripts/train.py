e639f9e66c9ecb80d3e4da619c98672d574c7064
UrbanPistek <urban.pistek@gmail.com>
2023-03-05 19:18:28 -0500

    Updated train script to handle multiple calibration files

--- plastic-identifier/scripts/train.py
+++ plastic-identifier/scripts/train.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 import time
 import json
@@ -22,45 +23,32 @@ from lib.postprocess import SpectraGen
 LEDS = [850, 940, 1050, 890, 1300, 880, 1550, 1650]
 
 # Locations of data
-DATA_DIR = "./data"
+DATA_DIR = "./data/dataset1"
 TRAIN_DIR = DATA_DIR + "/train"
 VAL_DIR = DATA_DIR + "/val"
+CALIBRATION_FILES = [
+    "/home/urban/urban/uw/fydp/3dentification/plastic-identifier/scripts/data/dataset1/bv1_id1_daytime_calibration_2023_03_05.csv",
+    "/home/urban/urban/uw/fydp/3dentification/plastic-identifier/scripts/data/dataset1/bv1_id2_late_afternoon_calibration_2023_03_05.csv",
+]
 
-def init_spectra_cal_ref(S: SpectraGen) -> None:
+# Directories for each category
+# LABEL_DIRS = ["/abs", "/pla", "/empty", "/other"]
+# LABELS = [0, 1, 2, 3]
+LABEL_DIRS = ["/abs", "/pla"]
+LABELS = [0, 1]
+
+# For regex
+CALI_ID_REGEX =  r"_id(\d+)_"
+
+def init_spectra_cal_ref(S: SpectraGen, calibration_file: str) -> None:
 
     # Read data
-    df_ref = pd.read_csv("./data/bv1_reference_compiled.csv")
-    df_cali = pd.read_csv("./data/bv1_open_calibration.csv")
-    df_ref.rename(columns={"Unnamed: 0": "units"}, inplace=True)
-    df_cali.rename(columns={"Unnamed: 0": "units"}, inplace=True)
+    df_cali = pd.read_csv(calibration_file)
 
-    ref = S.subtract_noise(df=df_ref)
+    # Remove noise and add calibration
+    df_cali.rename(columns={"Unnamed: 0": "units"}, inplace=True)
     cali = S.subtract_noise(df=df_cali)
     S.add_calibration_values(cali)
-    S.add_reference_values(ref)
-
-def gen_datasets(Spectra: SpectraGen) -> np.ndarray:
-    
-    # training dataset
-    train_x, train_y = [], []
-    train_files_pla = glob.glob(TRAIN_DIR + "/pla/**/*.csv")
-    train_files_abs = glob.glob(TRAIN_DIR + "/abs/**/*.csv")
-    
-    test_x, test_y = [], []
-    test_files_pla = glob.glob(VAL_DIR + "/pla/*.csv")
-    test_files_abs = glob.glob(VAL_DIR + "/abs/*.csv")
-
-    # add pla
-    # pla = 0
-    # abs = 1
-    extract_data(train_x, train_y, Spectra, 0, train_files_pla)
-    extract_data(test_x, test_y, Spectra, 0, test_files_pla)
-
-    # add abs
-    extract_data(train_x, train_y, Spectra, 1, train_files_abs)
-    extract_data(test_x, test_y, Spectra, 1, test_files_abs)
-
-    return np.array(train_x), np.array(train_y), np.array(test_x), np.array(test_y)
 
 def extract_data(x: list, y: list, S: SpectraGen, label: int, glob_files): 
 
@@ -68,21 +56,48 @@ def extract_data(x: list, y: list, S: SpectraGen, label: int, glob_files):
         df = pd.read_csv(file)
         df.rename(columns={"Unnamed: 0": "units"}, inplace=True)
         S.add_measurements(df)
-        S.filtered_spectra()
-        ys = S.normalize()
+        ys = S.filtered_spectra()
 
         x.append(ys)
         y.append(label)
+
+def merge_data(Spectra: SpectraGen, directory: str, label: int, calibrationId: int, x: list, y: list):
+
+    # Grab all files in the directory according to a specific calibration id
+    files = glob.glob(directory + f"/**/*_id{calibrationId}_*.csv", recursive=True)
+    extract_data(x, y, Spectra, label, files)
+
+def gen_datasets(Spectra: SpectraGen) -> np.ndarray:
+    
+    # training dataset
+    train_x, train_y = [], []    
+    test_x, test_y = [], []
+
+    for calibration in CALIBRATION_FILES:
+
+        # Get calibration id
+        substring = re.search(CALI_ID_REGEX, calibration)
+        cali_id = substring.group(1)
+
+        # Initialize spectra object
+        init_spectra_cal_ref(Spectra, calibration_file=calibration)
+
+        # Merge data according to calibration ids
+        # Run for training
+        for i, dir in enumerate(LABEL_DIRS):
+            merge_data(Spectra, TRAIN_DIR+dir, i, cali_id, train_x, train_y)
+
+        # Run for validation
+        for i, dir in enumerate(LABEL_DIRS):
+            merge_data(Spectra, VAL_DIR+dir, i, cali_id, test_x, test_y)
+
+    return np.array(train_x), np.array(train_y), np.array(test_x), np.array(test_y)
 
 def main():
     print("Spectra Classifier Training...")
     
     # Spectra generation object
     Spectra = SpectraGen(led_wavelengths=LEDS)
-
-    # Initialize spectra object
-    init_spectra_cal_ref(Spectra)
-    Spectra.display()
 
     train_x, train_y, test_x, test_y = gen_datasets(Spectra)
     print(f"\ntrain_x: {train_x.shape}, train_y: {train_y.shape}, test_x: {test_x.shape}, test_y: {test_y.shape}")
@@ -94,8 +109,8 @@ def main():
     ]
 
     classifiers = [
-        DecisionTreeClassifier(max_depth=25),
-        RandomForestClassifier(max_depth=15, n_estimators=10, max_features=1),
+        DecisionTreeClassifier(max_depth=250),
+        RandomForestClassifier(max_depth=250, n_estimators=50, max_features=8),
         AdaBoostClassifier(),
     ]
 
